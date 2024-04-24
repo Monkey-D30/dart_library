c7db9ff80ce171d0c45a2de2dc8d96360ac416c5
Arga <gokumonkeyd91@gmail.com>
2024-04-24 12:04:33 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:dart_library/hello.dart';
+
+void main() {
+  print(sayHello("Arga"));
+
+  Customer customer = Customer();
+  Category category = Category();
+  Product product = Product();
+}
